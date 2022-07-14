09c97540b53c86176956abca94dd19302ada0b11
xkc520 <102268753+xkc520@users.noreply.github.com>
2022-07-14 15:14:11 +0800

    Update dada.java

--- src/main/java/aa/dada.java
+++ src/main/java/aa/dada.java
@@ -4,7 +4,6 @@ public class dada {
     public static void main(String[] args) {
         System.out.println("???");
         System.out.println("hellp");
-    System.out.println("!!!!");
-        System.out.println("aa");
+  
     }
 }
